438fbabb4c7870f039311f46a5f28bf39ca03ae0
C19343861 <simonap0123@gmail.com>
2026-04-06 22:24:25 +0100

    UI updates

--- housing_affordability_lbs/frontend/src/App.jsx
+++ housing_affordability_lbs/frontend/src/App.jsx
@@ -195,7 +195,7 @@ export default function App() {
         },
 
         click: () => {
-          setSelectedKey(item.key);
+          setSelectedKey(key);
           setActivePanelTab("selected");
           setPanelOpen(true);
         }
